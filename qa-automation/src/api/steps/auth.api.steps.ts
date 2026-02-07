4018739b64310867e21610568f6786cb9f2947c1
GPYW <pasindiyathra@gmail.com>
2026-02-07 18:33:50 +0530

    addCommonAuthUtill

--- qa-automation/src/api/steps/auth.api.steps.ts
+++ qa-automation/src/api/steps/auth.api.steps.ts
@@ -6,31 +6,17 @@ import { login } from "../clients/auth.client.js";
 import { ENV } from "../../config/env.js";
 import type { APIWorld } from "../support/world.js";
 import { jwtDecode } from "jwt-decode";
+import {
+  authenticateAdmin,
+  authenticateUser,
+} from "../pretests/authpretests.js";
 
 Given("Admin authenticated", async function (this: APIWorld) {
-  const creds = ENV.USERS.admin;
-  this.lastResponse = await login(creds.username, creds.password);
-
-  const response = this.lastResponse;
-  expect(response?.status).toBe(200);
-  if (!response) throw new Error("Expected login response");
-
-  const data = response.data as Record<string, unknown>;
-  this.authToken = (data.token as string) ?? null;
-  expect(this.authToken).toBeTruthy();
+  await authenticateAdmin(this);
 });
 
 Given("User authenticated", async function (this: APIWorld) {
-  const creds = ENV.USERS.user;
-  this.lastResponse = await login(creds.username, creds.password);
-
-  const response = this.lastResponse;
-  expect(response?.status).toBe(200);
-  if (!response) throw new Error("Expected login response");
-
-  const data = response.data as Record<string, unknown>;
-  this.authToken = (data.token as string) ?? null;
-  expect(this.authToken).toBeTruthy();
+  await authenticateUser(this);
 });
 
 When(
